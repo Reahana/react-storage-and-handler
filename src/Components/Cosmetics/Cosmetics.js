b06cfbfc442d32113991f6f0449a448e174df814
Reahana <reahana233@gmail.com>
2023-07-14 11:12:42 +0600

    cosmetics

---
+++ src/Components/Cosmetics/Cosmetics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Cosmetics = () => {
+  
+    return (
+        <div>
+            <h1>Welcome to my Cosmetics Store</h1>
+            
+        </div>
+    );
+};
+
+export default Cosmetics;
