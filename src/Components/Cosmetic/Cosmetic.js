f196367af5a9b454c08069260d3e2892abd4e50c
Reahana <reahana233@gmail.com>
2023-07-14 12:48:04 +0600

    remove  from db

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './Cosmetic.css';
-import { addToDb } from '../../Utilities/Fakedb';
+import { addToDb, removeFromDb } from '../../Utilities/Fakedb';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
  const addToCart=(id) =>{
     addToDb(id)
  }
+
+ const removeFromCart = id =>{
+    removeFromDb(id)
+}
     return (
         <div className='product'>
              <h2>Buy this: {name}</h2>
             <p>Only for:  ${price}</p>
             <p><small>It has id {id}</small></p>
             <button onClick={() => addToCart(id)} >Add to Cart</button>
-            <button >Remove</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
         </div>
     );
 };
--- src/Utilities/Fakedb.js
+++ src/Utilities/Fakedb.js
@@ -18,17 +18,26 @@ const addToDb = id => {
      shoppingCart[id] = 1;
  }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-
-
-
    
 }
 
 
+const removeFromDb = id =>{
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        const shoppingCart = JSON.parse(storedCart);
+        if(id in shoppingCart){
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
+    }
 
+    
+}
 
 
 
 export {
-    addToDb
+    addToDb,
+    removeFromDb
 }
